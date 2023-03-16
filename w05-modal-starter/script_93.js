eb1448398a0684a2d1c4572e8ff7ca851e6ef2df
Jun206 <209410793@gms.tku.edu.tw>
2023-03-16 22:06:50 +0800

    W05-P5:  press 3 different buttons can show 3 different modals

--- w05-modal-starter/script_93.js
+++ w05-modal-starter/script_93.js
@@ -1,32 +1,36 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
+const modal = document.querySelectorAll('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelectorAll('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log('modal', modal);
-console.log('show-modal', btnOpenModal);
+console.log('show-modal', btnsOpenModal);
 
-const openModal = () => {
-    modal.classList.remove('hidden');
+const closeModal = () => {
+  modal.forEach((item) => {
+    item.classList.add('hidden');
+  });
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', () => {
+    modal[i].classList.remove('hidden');
     overlay.classList.remove('hidden');
+  });
 }
 
-const closeModal = ()  => {
-    modal.classList.add('hidden')
+for (let i = 0; i < btnCloseModal.length; i++) {
+  btnCloseModal[i].addEventListener('click', () => {
+    modal[i].classList.add('hidden');
     overlay.classList.add('hidden');
+  });
 }
-
-
-for( let i=0; i<btnOpenModal.length; i++) {
-    btnOpenModal[i].addEventListener('click', openModal);
-}
-
-btnCloseModal.addEventListener('click', closeModal );
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-        closeModal();
-    }
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
 });
