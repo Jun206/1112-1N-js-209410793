6817d61130940aeff8e39e1d80ce3fe5398d3cc6
Jun206 <209410793@gms.tku.edu.tw>
2023-05-25 20:59:48 +0800

    W15-P1: prepare menu items for API project in Vercel

---
+++ demo/w15-store-product/js/app_local.js
@@ -0,0 +1,47 @@
+const url = `./api/products.json`;
+
+const pContainer = document.querySelector('.products-container');
+
+let allProducts; 
+let products;
+
+const fetchData = async ()=>{
+    try {
+        const response = await fetch(url)
+        const data = response.json();
+        console.log('fetchData',data);
+        return data; 
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const displayProducts = (products)=>{
+    let displayContent = products.map((product, index)=>{
+        const{company, name,price} = product.fields;
+        let image = `./images/product-${index+1}.jpg`;
+        console.log(image);
+        return`
+            <div class="single-product">
+            <img
+              src="${image}"
+              class="single-product-img img"
+              alt="high-back bench"
+            />
+            <footer>
+              <h5 class="name">${name}</h5>
+              <span class="price">$${price/100}</span>
+            </footer>
+          </div>
+        `
+    }).join('');
+
+    // console.log('displayContent',displayContent);
+    pContainer.innerHTML = displayContent;
+}
+
+window.addEventListener('DOMContentLoaded',async()=>{
+    allProducts = await fetchData();
+    console.log('allProduct' ,allProducts);
+    await displayProducts(allProducts);
+});
