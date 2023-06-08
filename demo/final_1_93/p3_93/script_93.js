a834026a2fe82309feac527cbc1d7bf4e09eb416
Jun206 <209410793@gms.tku.edu.tw>
2023-06-08 21:59:21 +0800

    P3: API Projects and DOM projects

--- demo/final_1_93/p3_93/script_93.js
+++ demo/final_1_93/p3_93/script_93.js
@@ -2,101 +2,115 @@ const search = document.querySelector('#search');
 const submit = document.querySelector('#submit');
 const random = document.querySelector('#random');
 const resultHeading = document.querySelector('#result-heading');
-const mealsEl = document.querySelector('#meals');
-const single_mealEl = document.querySelector('#single-meal');
+const cocktailsEl = document.querySelector('#cocktails');
+const single_cocktailsEl = document.querySelector('#single-cocktail');
 
-const searchMeal = (e) => {
-  e.preventDefault();
-  const term = search.value;
-  //   console.log('term', term);
-  if (term.trim()) {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('meals data', data);
 
-        if (data.meals === null) {
-          resultHeading.innerHTML = `<p> There are no search results. Try again! </p>`;
-        } else {
-          resultHeading.innerHTML = `<h2> Search Results for '${term}' </h2>`;
-          mealsEl.innerHTML = data.meals
-            .map((meal) => {
-              return `
-                  <div class="meal">
-                    <img src="${meal.strMealThumb}" />
-                    <div class="meal-info" data-mealid="${meal.idMeal}">
-                       <h3> ${meal.strMeal} </h3>
-                    </div>
-                  </div>
-                `;
+
+const searchCocktail = (e)=>{
+    e.preventDefault();
+    const term = search.value;
+//     console.log('term',term);
+    if(term.trim()){
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${term}`)
+            .then( response =>response.json())
+            .then(data =>{
+                console.log('data',data);
+
+                if(data.cocktails === null){
+                    resultHeading.innerHTML = `<p> There are no search results. Try again! </p>`
+                }else{
+                    resultHeading.innerHTML = `<h2> Search Results for ${term} </h2>`
+                    cocktailsEl.innerHTML = data.cocktails.map((cocktail)=>{
+                        return`
+                            <div class="cocktail">
+                                <img src="${cocktail.strCocktailThumb}" />
+                                <div class="cocktail-info" data-cocktailID="${cocktail.idCocktail}">
+                                    <h3>${cocktail.strCocktail}</h3>
+                                </div>
+                            </div>
+                        `
+                    }).join('');
+                }
             })
-            .join('');
-        }
-      });
-  } else {
-    // alert('Please enter a search term');
-  }
-};
-
-const getMealById = (mealID) => {
-  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
-    .then((resp) => resp.json())
-    .then((data) => {
-      console.log('single meal', data);
-      const meal = data.meals[0];
-      addMealToDOM(meal);
-    });
-};
-
-const addMealToDOM = (meal) => {
-  const ingredients = [];
-  for (let i = 1; i <= 20; i++) {
-    if (meal[`strIngredient${i}`]) {
-      ingredients.push(
-        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
-      );
-    } else {
-      break;
+    }else{
+        // alert('Please enter a search term');
     }
-  }
-  console.log('ingredients', ingredients);
-
-  single_mealEl.innerHTML = `
-      <div class="single-meal">
-        <h1> ${meal.strMeal} </h1>
-        <img src="${meal.strMealThumb}" />
-        <div class="main">
-           <p> ${meal.strInstructions} </p>
-           <h2> Ingredients </h2>
-           <ul>
-             ${ingredients
-               .map((ing) => {
-                 return `<li>${ing}</li>`;
-               })
-               .join('')}
-           </ul>
-        </div>
-
-      </div>
-   `;
 };
+
+const getCocktailById = (cocktailID)=>{
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailID}`)
+    .then(resp => resp.json())
+    .then(data =>{
+        console.log('cocktail data',data);
+        const cocktail = data.cocktails[0];
+        addCocktailToDOM(cocktail);
+    })
+};
+
+const addCocktailToDOM = (cocktail)=>{
+    const ingredients = [];
+    for(let i = 1;i <=20;i++){
+        if(cocktail[`strIngredient${i}`]){
+            ingredients.push(`${cocktail[`strIngredient${i}`]} - ${cocktail[`strMeasure${i}`]}`);
+        }else{
+            break;
+        }
+    }
+    console.log('ingredients',ingredients);
+
+    single_cocktailsEl.innerHTML = `
+    <div class="single-cocktail">
+        <h1> ${cocktail.strCocktail} </h1>
+        <img src="${cocktail.strCocktailThumb}">
+        <div class="main">
+            <p> ${cocktail.strInstructions} </p>
+            <h2> Ingredients </h2>
+            <ul>
+                ${ingredients.map(ing =>{
+                    return `<li> ${ing} </li>`
+                }).join('')}
+            </ul>
+        </div>
+    </div>
+    `
+};
+
+
 
 // Event listeners
-submit.addEventListener('click', searchMeal);
+submit.addEventListener('click',searchCocktail);
 
-mealsEl.addEventListener('click', (e) => {
-  // console.log('e.path', e.composedPath());
-  const composedPath = e.composedPath();
-  const mealInfo = composedPath.find((item) => {
-    if (item.classList) {
-      return item.classList.contains('meal-info');
-    } else {
-      return false;
+cocktailsEl.addEventListener('click', e =>{
+    // console.log('e.path',e.composedPath());
+    const composedPath = e.composedPath();
+    const cocktailInfo = composedPath.find(item=>{
+        if(item.classList){
+            return item.classList.contains('cocktail-info');
+        }else{
+            return false;
+        }
+    });
+    if (cocktailInfo){
+        console.log('cocktailInfo',cocktailInfo);
+        const cocktailID = cocktailInfo.getAttribute('data-cocktailid');
+        getCocktailById(cocktailID);
     }
-  });
-  if (mealInfo) {
-    // console.log('mealInfo', mealInfo);
-    const mealID = mealInfo.getAttribute('data-mealid');
-    getMealById(mealID);
-  }
 });
+
+
+
+
+
+const randomBtn = () => {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log('cocktail data', data);
+            const cocktail = data.cocktails[0];
+            addCocktailToDOM(cocktail);
+        });
+};
+
+
+random.addEventListener('click', randomBtn);
