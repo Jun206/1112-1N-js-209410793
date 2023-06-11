1f9c1428bb7a7e5c4c3a60b03705d7f44996289d
Jun206 <209410793@gms.tku.edu.tw>
2023-06-11 10:43:46 +0800

    W14-Finsh

--- demo/w14-api_more/app_93.js
+++ demo/w14-api_more/app_93.js
@@ -1,14 +1,14 @@
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', async () => {
-  const url = 'https://theaudiodb.com/api/v1/json/2/album.php?i=112024';//注意要打上http://
+  const url = 'https://www.thesportsdb.com/api/v1/json/3/lookupplayer.php?id=34145937';
 
 
 try {
 	const response = await fetch(url);
 	const data = await response.json();
-	console.log('data', data.album);
-  displayItems(data.album);
+	console.log('data', data.players);
+  displayItems(data.players);
 } catch (error) {
 	console.error(error);
 }
@@ -17,8 +17,8 @@ try {
 const displayItems = (items) => {
   const displayData = items
     .map((item) => {
-      const { strAlbum } = item;
-      return `<p>${strAlbum}</p>`;
+      const { strPlayer } = item;
+      return `<p>${strPlayer}</p>`;
     })
     .join('');
   const element = document.createElement('div');
